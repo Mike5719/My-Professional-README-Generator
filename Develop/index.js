8916ca229443b0b854c7833838147ef7190681e3
Michael Girgis <michael.girgis@live.ca>
2023-12-07 19:06:10 -0500

    updates

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,84 @@
-// TODO: Include packages needed for this application
+//node modules
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      message: 'What is your project title?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Provide a short description explaining the what, why, and how of your project.',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'What are the steps required to install your project?',
+      name: 'Installation',
+    },
+    { type: 'input',
+      message: 'How is your project used?',
+      name: 'Usage',
+    },
+    { type: 'input',
+      message: 'What are the guidelines for contribution?',
+      name: 'Contributing',
+    },
+    { type: 'checkbox',
+      name: 'license',
+      message: 'Which license would you like to use?',
+      choices: ['Apache License 2.0','GNU General Public License','MIT License','Mozilla Public License 2.0','The Unlicense'],
+    },  
+    { type: 'input',
+      message: 'How can your project be tested?',
+      name: 'Tests',
+    },
+    { type: 'input',
+      message: 'What is your GitHub username?',
+      name: 'Github',  
+    },
+    { type: 'input',
+      message: 'What is your email address?',
+      name: 'email',
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  ];
 
-// TODO: Create a function to initialize app
-function init() {}
+  function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("You're Professional README file is ready to view!");
+    });
+  };
 
-// Function call to initialize app
-init();
+  function init() {
+    inquirer.prompt(questions)
+    .then(function(userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+  };
+
+  init();
+
+
+
+
+// // TODO: Include packages needed for this application
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,8 +13,33 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## License:
+  [![License] (https://img.shields.io/badge/license-${data.license}-blue.svg)]
+  ##Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
 
-`;
+  ## Description:
+  ${data.description}
+  ## Installation:
+  ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## License
+  ${data.license}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: [${data.email}](mailto:user@example.com)';
+
 }
 
 module.exports = generateMarkdown;
