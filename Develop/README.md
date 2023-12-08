632fad3627876badbf8731c9b7f2fac5d028f7e8
Michael Girgis <michael.girgis@live.ca>
2023-12-07 22:05:28 -0500

    updates

---
+++ Develop/README.md
@@ -0,0 +1,28 @@
+# help
+  ## License:
+  
+  [![license](https://img.shields.io/badge/license-Apache License 2.0-blue.svg)](https://shields.io/)
+  ##Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Description:
+  please
+  ## Installation:
+  this
+  ## Usage:
+  is 
+  ## License
+  Look for the project under Apache License 2.0
+  
+  ## Contributing
+  so 
+  ## Tests
+  boring
+  ## Questions
+  - Github: [and](https://github.com/and)
+  - Email: [tedious](mailto:user@example.com)
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,25 +1,52 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (this.license === 'true') {
+    return `[![license:MIT](https://img.shields.io/badge/license-${data.license}-blue.svg)](https://shields.io/)`
+  } else {
+    return ''
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license === 'Apache License 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  } else if (license === 'GNU General Public License') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  
+  } else if (license === 'MIT License') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  
+  } else {
+    return ''
+  }
+
+}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `Look for the project under ${license}`
+  } else {
+    return "Please choose a license."
+  }
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ## License:
-  [![License] (https://img.shields.io/badge/license-${data.license}-blue.svg)]
+  ${renderLicenseBadge(data.license)}
+  [![license](https://img.shields.io/badge/license-${data.license}-blue.svg)](https://shields.io/)
   ##Table of Contents
   - [Description](#description)
   - [Installation](#installation)
   - [Usage](#usage)
-  - [License](#license)
   - [Contributing](#contributing)
   - [Tests](#tests)
   - [Questions](#questions)
@@ -31,14 +58,15 @@ function generateMarkdown(data) {
   ## Usage:
   ${data.usage}
   ## License
-  ${data.license}
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
   ## Contributing
   ${data.contributing}
   ## Tests
   ${data.tests}
   ## Questions
   - Github: [${data.github}](https://github.com/${data.github})
-  - Email: [${data.email}](mailto:user@example.com)';
+  - Email: [${data.email}](mailto:user@example.com)`;
 
 }
 
